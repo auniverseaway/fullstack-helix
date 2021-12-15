d71658c23f222de5095ee21ef90897ef3887151c
Chris Millar <cmillar@adobe.com>
2021-12-15 13:14:06 -0700

    foo

--- scripts.js
+++ scripts.js
@@ -1,2 +1,26 @@
 const hero = document.querySelector('.hero');
-console.log(hero);
+
+const content = hero.querySelector(':scope > div > div');
+content.classList.add('content-container');
+
+
+function decorateAccordion(el) {
+    const titles = el.querySelectorAll(':scope > div:nth-child(odd)');
+    titles.forEach((title) => {
+      // Add a class to the title container
+      title.classList.add('item-title');
+      // Remove the empty div
+      title.querySelector(':scope > div:last-of-type').remove();
+      // Add a class to the content
+      title.nextElementSibling.classList.add('item-content');
+      // Add a click handler to open the content
+      title.addEventListener('click', () => {
+        title.classList.toggle('open');
+      });
+    });
+  }
+  
+  const els = document.querySelectorAll('.accordion');
+  els.forEach((el) => {
+    decorateAccordion(el);
+  });
